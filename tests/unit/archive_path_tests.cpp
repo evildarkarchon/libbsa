30ed55f8ae05b806b6ee0d01bb91cd8496031ad3
evildarkarchon <evildarkarchon@gmail.com>
2026-05-07 21:44:27 -0700

    test(02-02): add failing archive path tests
    - specify canonical lowercase forward-slash keys
    - reject invalid virtual path forms

---
+++ tests/unit/archive_path_tests.cpp
@@ -0,0 +1,30 @@
+#include <catch2/catch_test_macros.hpp>
+
+#include <detail/archive_path.hpp>
+
+#include <array>
+#include <string_view>
+
+TEST_CASE("archive_path normalizes separators and ASCII case", "[unit][archive-path]") {
+  auto key = libbsa::detail::normalize_archive_path("Meshes\\Foo/BAR.NIF");
+
+  REQUIRE(key);
+  REQUIRE(key.value().value == "meshes/foo/bar.nif");
+}
+
+TEST_CASE("archive_path rejects obvious invalid virtual paths", "[unit][archive-path][malformed]") {
+  constexpr auto invalid_paths = std::to_array<std::string_view>({
+      "",
+      "/absolute/path",
+      "C:/drive/rooted",
+      "textures//bad.dds",
+      "textures/./bad.dds",
+      "textures/../bad.dds",
+  });
+
+  for (const auto input : invalid_paths) {
+    auto key = libbsa::detail::normalize_archive_path(input);
+    REQUIRE_FALSE(key);
+    REQUIRE(key.error().code == libbsa::error_code::invalid_argument);
+  }
+}
